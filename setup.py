9618775082084baa3216d2e55dede457e62a7512
Danilo Weber Nunes <danilownunes@gmail.com>
2022-03-14 21:11:29 +0100

    Removed unused requirements.txt and fixed package from git installation.

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements.txt", "r") as f:
-    requirements = f.readlines()
-
 setup(
     name="transformations2log",
     version="0.0.1",
-    description="Simple class to make Pytorch dataset object creation easier and more flexible.",
+    description=" A simple package to extract torchvision transformations used during training and testing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Danilo Weber Nunes",
     author_email="danilownunes@gmail.com",
     url="https://github.com/danilown/transformations2log",
     license="MIT",
-    install_requires=requirements,
     packages=find_packages(),
 )
